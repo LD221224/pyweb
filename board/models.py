96c890abb69fcd039009d3ed45203aab36252301
LD221224 <dayeong2815@gmail.com>
2022-03-11 12:36:59 +0900

    day46, 추천

--- board/models.py
+++ board/models.py
@@ -7,7 +7,8 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)  # null 허용, blank - 폼 데이터 null
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천수 - 다대다 관계
 
     def __str__(self):
         return self.subject
